a3a8b668413ca1df08455e1fee5955f96ae9cc63
David Babickovic PC <davidbabickovic@gmail.com>
2026-07-16 14:43:49 +0200

    Task with IF

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 name = "User"
 age = 20
-goal = "Programer"
+goal = "Programmer"
 #print("hey my name is: "+name)
 #print("i am "+str(age)+" years old")
 #print("i want to be a "+goal)
@@ -11,4 +11,13 @@ goal = "Programer"
 #print(f"i am {age} years old")
 #print(f"i want to be a {goal}")
 
-name = input("Whats your name?")
+#name = input("Whats your name? :")
+#age = input("How old are you? :")
+#print(f"Hello {name} !")
+#print(f"Next year you will be {int(age)+1} years old.")
+
+age = int(input("how old are you? "))
+if age < 18:
+    print("You are underage.")
+else:
+    print("You are an adult.")
