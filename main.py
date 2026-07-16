5241ddd3fc645692340ec562917968e818f640b1
David Babickovic <davidbabickovic@Davids-MacBook-Air.local>
2026-07-16 14:09:26 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+name = "User"
+age = 20
+goal = "Programer"
+#print("hey my name is: "+name)
+#print("i am "+str(age)+" years old")
+#print("i want to be a "+goal)
+
+#print(f"my name is {name}")
+#print(f"i am {age} years old")
+#print(f"i want to be a {goal}")
+
+name = input("Whats your name?")
