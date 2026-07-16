3474a533dfd5d966a12e2398d1181c4dd3274a9f
David Babickovic PC <davidbabickovic@gmail.com>
2026-07-16 15:43:44 +0200

    Lesson03 Complete for loop exercises

---
+++ Lesson03/main.py
@@ -0,0 +1,12 @@
+
+
+# for number in range(1,11):
+#     print(number)
+# print("")    
+# for number in range(2,21,2):
+#     print(number)
+# print("")
+
+user_number = int(input("Enter a number: "))
+for number in range(1,11):
+    print(f"{user_number} * {number} = {user_number*number}")
